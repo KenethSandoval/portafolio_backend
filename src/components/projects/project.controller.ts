8ed224660b88e16034f499fc0975d8836dd8bd50
KenethSandoval <kenethra.74@gmail.com>
2021-09-03 15:16:36 -0600

    feat: get by id

--- src/components/projects/project.controller.ts
+++ src/components/projects/project.controller.ts
@@ -2,9 +2,23 @@ import { Projects } from "./project.model";
 
 export function getAll(): LazyList<IProjectSchema[]> {
   return async (): Promise<any> => {
-    const project: IProjectSchema[] = await Projects.find({});
-    if (project.length === 0) {
+    const projects: IProjectSchema[] = await Projects.find({});
+    if (projects.length === 0) {
       throw { code: 404, message: "Void" };
+    } else {
+      return {
+        allData: () => projects,
+      };
+    }
+  };
+}
+
+export function getById(id: string): LazyList<IProjectSchema[] | null> {
+  return async (): Promise<any> => {
+    const project: IProjectSchema | null = await Projects.findById(id);
+
+    if (!project) {
+      throw { code: 404, message: "User not found" };
     } else {
       return {
         allData: () => project,
