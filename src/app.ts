d3858e18e767e358660548b596dcd083f212f480
Стив <kenethra.74@gmail.com>
2022-06-28 19:51:24 -0600

    make it better

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application } from "express";
 import cors from "cors";
+import express, { Application } from "express";
 import { connect } from "mongoose";
 import { router as routerAuth } from "./components/auth/auth.route";
 import { router as routerProject } from "./components/projects/project.route";
@@ -15,13 +15,16 @@ app.use(cors());
 app.use("/auth", routerAuth);
 app.use("/project", routerProject);
 
-connect("mongodb://localhost:27017/portafolio", () => {
-  console.log(">>DB online");
-});
+connect(
+	"mongodb+srv://ksandoval:15052002@cluster0.x3sdc.mongodb.net/?retryWrites=true&w=majority",
+  /*connect("mongodb://localhost:27017/portafolio"*/() => {
+		console.log(">> DB online");
+	}
+);
 
 if (process.env.NODE_ENV !== "test")
 	app.listen(process.env.PORT, () => {
-  	console.log(`Server on listen http://localhost:${process.env.PORT}`);
+		console.log(`Server on listen http://localhost:${process.env.PORT}`);
 	});
 
 export default app;
